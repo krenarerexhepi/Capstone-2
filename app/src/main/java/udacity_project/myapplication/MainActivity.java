8c24763448b125aa2bed261b1b3a56fc7adee578
krenarerexhepi <krenare@outlook.com>
2016-09-15 23:46:35 +0200

    Corrected one bug

--- app/src/main/java/udacity_project/myapplication/MainActivity.java
+++ app/src/main/java/udacity_project/myapplication/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getBaseContext(), AddDrinkActivity.class);
                 intent.putExtra(EXTRA_MESSAGE, username);
+                intent.putExtra("EXTRA_ID","");
                 startActivity(intent);
             }
         });
